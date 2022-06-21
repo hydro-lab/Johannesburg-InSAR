d280746a5876f5612dce905223be247101743bdb
WMTrainor <90650952+WMTrainor@users.noreply.github.com>
2022-06-21 11:25:31 -0400

    First Iteration
    First one failed, trying a new EPSG and coordinates

---
+++ Reproject_resample_clip.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+import glob
+from osgeo import gdal
+
+ls = (glob.glob("/Volumes/hydro3-raid/Will_InSAR/hype/*/*_clip.tif"))
+
+for fn in ls:
+    split = fn.split(".")
+    out = split[0]+"_rpclip.tif"
+    shpin = "/Volumes/hydro3-raid/Will_InSAR/Will_InSAR_Study_Area/Johannesburg_Study_Area.shp"
+    gdal.Warp(out,fn,srcSRS='EPSG:32617',dstSRS='EPSG:4326', xRes = 0.000766570302388142, yRes = -0.000766570302388142, cutlineDSName = shpin, cropToCutline = True)
