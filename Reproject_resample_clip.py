aee9c0ceca0358d4c37af2fc0bbcd058a1b23c3a
WMTrainor <90650952+WMTrainor@users.noreply.github.com>
2022-06-23 14:55:24 -0400

    Changed UPSG code

--- Reproject_resample_clip.py
+++ Reproject_resample_clip.py
@@ -9,4 +9,4 @@ for fn in ls:
     split = fn.split(".")
     out = split[0]+"_rpclip.tif"
     shpin = "/Volumes/hydro3-raid/Will_InSAR/Will_InSAR_Study_Area/Johannesburg/Johannesburg_Study_Area.shp"
-    gdal.Warp(out,fn,srcSRS='EPSG:22293',dstSRS='EPSG:4326', xRes = 0.000766570302388142, yRes = -0.000766570302388142, cutlineDSName = shpin, cropToCutline = True
+    gdal.Warp(out,fn,srcSRS='EPSG:32735',dstSRS='EPSG:4326', xRes = 0.000766570302388142, yRes = -0.000766570302388142, cutlineDSName = shpin, cropToCutline = True)
